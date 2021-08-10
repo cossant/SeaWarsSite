7e4083b26d97fbbad43a458605a60c379d3ec8a1
Антон Костюченко <Anton@Kostyuchenko.com>
2021-08-10 16:30:16 +0400

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs"  Inherits="SeaWarsSite.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server" Height="16px" Width="1289px" Visible="False" ></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Начать новую игру" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Подтвердить (Ептег)" Visible="False" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SeaWarsSite
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        // Need to write the new dll directory to continue tests.
+        [DllImport("Dll_Wars.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern IntPtr initController();
+        // "C:/Users/Anton/source/repos/Dll_Wars/Debug/Dll_Wars.dll"
+        [DllImport("Dll_Wars.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern int manageInput(StringBuilder dialogueBuffer, int length , IntPtr controller);
+
+        StringBuilder stringB;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "Це морской бой, братишка. И я его таки докрутил.";
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            Session["contr"] = initController();
+            Button1.Visible = false;
+            Button2.Visible = true;
+            TextBox1.Visible = true;
+            Button2_Click(Button1, e);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            stringB = new StringBuilder(TextBox1.Text);
+
+            // Normalisation.
+            while (stringB.Length < 256)
+                stringB.Append(' ');
+
+            manageInput(stringB, stringB.Length, (IntPtr)Session["contr"]);
+
+            Label1.Text = stringB.ToString();
+            if (Label1.Text.Contains("Игра завершается"))
+            {
+                Button2.Visible = false;
+                Button1.Visible = true;
+            }
+            stringB.Clear();
+            TextBox1.Text = "";
+        }
+    }
+}
